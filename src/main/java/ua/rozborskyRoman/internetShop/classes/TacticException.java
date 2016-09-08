c480de91e7d959cd99166d8f700fbaff9dcd643d
roman <roman.rozborsky@gmail.com>
2016-09-08 04:28:43 +0300

    add empty exception

---
+++ src/main/java/ua/rozborskyRoman/internetShop/classes/TacticException.java
@@ -0,0 +1,10 @@
+package ua.rozborskyRoman.internetShop.classes;
+
+/**
+ * Created by roman on 07.09.2016.
+ */
+public class TacticException extends RuntimeException {
+    public TacticException() {
+        System.out.println("exception");
+    }
+}
